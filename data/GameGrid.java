f3c8a6503366e597ff66c2c1fe8e8917f5aa91fc
superspindel <viktor.fallman@hotmail.com>
2016-02-22 16:24:29 +0100

    Update GameGrid.java

--- data/GameGrid.java
+++ data/GameGrid.java
@@ -1,163 +1,29 @@
-/*
-package lab4.data;
-
-import java.util.Observable;
-
-public class GameGrid extends Observable{
-	private int[][] gridArray;
-	private int size;
-	public static final int EMPTY = 0;
-	public static final int ME = 1;
-	public static final int OTHER = 2;
-	public static int INROW = 5;
-	
-
-	public GameGrid(int size)
-	{
-		this.size = size;
-		this.gridArray = new int[size][size];
-		this.clearGrid();
-	}
-
-	public int getLocation(int x, int y){
-		return this.gridArray[x][y];
-	}
-	
-
-	public int getSize(){
-		return this.size;
-	}
-	
-
-	public boolean move(int x, int y, int player){
-		if(this.gridArray[x][y] == EMPTY){
-			this.gridArray[x][y] = player;
-			ChangeNotify();
-			return true;
-		} 
-		else 
-		{
-			return false;
-		}
-	}
-	
-
-	public void clearGrid()
-	{
-		for(int i = 0; i<this.size; i++)
-		{
-			for(int k = 0; k<this.size; k++)
-			{
-				this.gridArray[i][k] = EMPTY;
-			}
-		}
-		ChangeNotify();
-	}
-	
-
-	public boolean isWinner(int player)
-	{
-		for(int i = 0; i<this.size; i++)
-		{
-			for(int k = 0; k<this.size; k++){
-				int winning = 0;
-				if(getLocation(i, k) == player){
-					for(int j = k; j<this.size; j++)
-					{
-						winning++;
-						if(getLocation(i, j) != player)
-						{
-							break;
-						}
-						else if(winning == INROW)
-						{
-							return true;
-						}
-					}
-					winning = 0;
-					for(int p = i; p<this.size; p++)
-					{
-						winning++;
-						if(getLocation(p, k) != player)
-						{
-							break;
-						}
-						else if(winning == INROW)
-						{
-							return true;
-						}
-					}
-					int diagUpX = i;
-					int diagUpY = k;
-					winning = 0;
-					while(diagUpX < this.size && diagUpY >= 0)
-					{
-						winning++;
-						if(getLocation(diagUpX, diagUpY) != player)
-						{
-							break;
-						}
-						else if(winning == INROW)
-						{
-							return true;
-						}
-						diagUpX++;
-						diagUpY--;
-					}
-					int diagDownX = i;
-					int diagDownY = k;
-					winning = 0;
-					while(diagDownX < this.size && diagDownY < this.size)
-					{
-						winning++;
-						if(getLocation(diagDownX, diagDownY) != player)
-						{
-							break;
-						}
-						else if(winning == INROW)
-						{
-							return true;
-						}
-						diagDownX++;
-						diagDownY++;
-					}
-				}
-			}
-		}
-		return false;
-	}
-	private void ChangeNotify()
-	{
-		setChanged();
-		notifyObservers();
-	}
-	
-	
-}
-*/
-
 package lab4.data;
 
 import java.util.Observable;
 
 /**
- * Represents the 2-d game grid
+ * 
+ * @author Jonas and Viktor
+ * represents the 2D grid for the game Gomoku.
+ * Has values for players that determine who has taken a square on the board.
+ * Value INROW for how many squares in a row needs to be taken to win.
+ *
  */
 
 public class GameGrid extends Observable {
 
-	public static int EMPTY = 0;
-	public static int ME = 1;
-	public static int OTHER = 2 ;
+	public static final int EMPTY = 0;
+	public static final int ME = 1;
+	public static final int OTHER = 2;
 	private int INROW = 5;
 	private int board[][];
-
-	// private boolean draw;
 	/**
 	 * Constructor
 	 * 
-	 * @param size
-	 *            The width/height of the game grid
+	 * @param size of the gaming board which is the width and height of the board.
+	 * will create a board and fill it with empty squares.
+	 *       
 	 */
 	public GameGrid(int size) {
 		board = new int[size][size];
@@ -199,8 +65,10 @@ public class GameGrid extends Observable {
 	 *            the x position
 	 * @param y
 	 *            the y position
-	 * @param player
-	 * @return true if the insertion worked, false otherwise
+	 * @param player is the player that wants to perform the move.
+	 * @return true if the insertion worked, false otherwhise.
+	 * If true, perform the move and setChanged and notifyObservers.
+	 * 
 	 */
 	public boolean move(int x, int y, int player) {
 
@@ -239,27 +107,34 @@ public class GameGrid extends Observable {
 		int counter = 0;
 		for (int x = 0; x < board.length; x++) {
 			for (int y = 0; y < board[x].length; y++) {
-				
 				counter = 0;
-				for (int i = 0; x + i< board.length;) {
+				for (int i = 0; x + i < board.length;) {
 					if(board[x + i++][y] == player){
 						counter++;
-						if (counter == INROW) {
+						if (counter == INROW) 
+						{
 							return true;
 						}
+					} 
+					else
+					{
+						break;
 					}
-					break;
-				
 				}
 				counter = 0;
 				for (int i = 0; y + i < board[x].length;) {
-					if(board[x][y + i++] == player){
+					if(board[x][y + i++] == player)
+					{
 						counter++;
 						if (counter == INROW) {
 							return true;
 						}
 					}
-					break;
+					else
+					{
+						break;
+					}
+
 				}
 			}
 		}
@@ -278,30 +153,35 @@ public class GameGrid extends Observable {
 		int counter = 0;
 		for (int x = 0; x < board.length; x++) {
 			for (int y = 0; y < board[x].length; y++) {
-				
 				counter = 0;
 				int z = 0;
-				for (int i = 0; x + i < board.length && y + z < board[x].length;) {
+				for (int i = 0; x + i < board.length && y + z < board.length;) {
 					if(board[x + i++][y + z++] == player){
 						counter++;
 						if (counter == INROW) {
 							return true;
 						}
 					}
-					break;
+					else
+					{
+						break;
+					}
 
 				}
 				counter = 0;
 				z = 0;
-				for (int i = 0; y + i < board[x].length && y + z < board[x].length;) {
-					if(board[x + i++][y - z++] == player){
+				for (int i = 0; y - z >= 0 && x+i < board.length;) {
+					if(board[x + i++][y - z++] == player)
+					{
 						counter++;
 						if (counter == INROW) {
 							return true;
 						}
 					}
-					break;
-					
+					else
+					{
+						break;
+					}
 				}
 			}
 		}
