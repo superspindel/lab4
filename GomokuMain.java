354e90dedd4796bed00043664c03fd07eb0c45c5
superspindel <viktor.fallman@hotmail.com>
2016-02-22 16:25:45 +0100

    Update GomokuMain.java

--- GomokuMain.java
+++ GomokuMain.java
@@ -3,6 +3,12 @@ import lab4.data.*;
 import lab4.client.*;
 import lab4.gui.*;
 
+/**
+ * 
+ * @author Viktor and Jonas
+ * Try and get the values from the arguments and parse them to int values, if not possible then set standard values
+ * setup two windows for a local client game, can use one of the windows for a online game or just comment out clientTwo.
+ */
 public class GomokuMain {
 	public static void main(String[] args){ //Port Number as argument.
 		int port1;
